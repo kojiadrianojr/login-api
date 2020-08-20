92fc85259acd61a36cef7ae03c148c810ab2dc15
kojiadrianojr <iisparkplugiiog@yahoo.com>
2020-08-20 13:01:48 +0800

    done otp api

--- app/src/pages/Login/OtpComponent.vue
+++ app/src/pages/Login/OtpComponent.vue
@@ -6,13 +6,7 @@
       </q-card-section>
       <q-form @submit="onSubmit">
         <q-card-section class="q-pt-none">
-          <q-input
-            dense
-            v-model="token"
-            label="Enter OTP"
-            autofocus
-            @keyup.enter="prompt = false"
-          />
+          <q-input dense v-model="token" label="Enter OTP" autofocus @keyup.enter="prompt = false" />
         </q-card-section>
 
         <q-card-actions align="right" class="text-primary">
@@ -41,25 +35,34 @@ export default {
   computed: {
     showPrompt: function () {
       return this.prompt;
-    }
+    },
   },
   methods: {
     async onSubmit(e) {
       e.preventDefault();
       try {
-        let auth = await this.$store.dispatch("auth/validate", {token: this.token, email: this.$store.state.auth.credentials.userInfo.email});
+        let auth = await this.$store.dispatch("auth/validate", {
+          token: this.token,
+          email: this.$store.state.auth.credentials.userInfo.email,
+        });
         console.log(auth);
         this.promptFn();
-        if (this.$store.state.auth.credentials.otp_auth){
-          return this.$router.push('/homepage')
+        if (this.$store.state.auth.credentials.otp_auth) {
+          this.$notify({
+            group: "auth",
+            title: "Access Granted",
+            text: "Enjoy your visit!",
+            type: "success",
+          });
+          return this.$router.push("/homepage");
         }
       } catch (e) {
         this.$notify({
-          group: 'auth',
-          title: 'Access Denied',
-          text: 'Please check your email for the OTOP',
-          type: 'error'
-        })
+          group: "auth",
+          title: "Access Denied",
+          text: "Please check your email for the OTOP",
+          type: "error",
+        });
       }
     },
   },
